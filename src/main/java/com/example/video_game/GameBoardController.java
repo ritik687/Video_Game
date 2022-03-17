df4e3718f4f1b97aea819048e1316a8c20457a54
ritik687 <200495974@student.georgianc.on.ca>
2022-03-17 04:45:27 -0400

    started with the controller class

--- src/main/java/com/example/video_game/GameBoardController.java
+++ src/main/java/com/example/video_game/GameBoardController.java
@@ -1,4 +1,15 @@
 package com.example.video_game;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class GameBoardController {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Button startButton;
+
 }
