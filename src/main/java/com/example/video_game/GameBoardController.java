7b0fae294a31cffc608b12cce3bb7013ffc72ca3
ritik687 <200495974@student.georgianc.on.ca>
2022-03-17 15:46:27 -0400

    sprite class is formed, done some validations not all

--- src/main/java/com/example/video_game/GameBoardController.java
+++ src/main/java/com/example/video_game/GameBoardController.java
@@ -26,13 +26,14 @@ public class GameBoardController {
         // we are going to use the tool called canvas or canvas class.... we draw on the canvas...Canvas needs a size, imagine if you are drawing on the canvas the size may be small and large..
         // there are two ways to set its dimension:- one ay to ask the anchor pane for its dimensions, thats the way to make it the exact same size && another way to do it is, we can set up a file for game constants
 
-        //scene builder is just the layout.... its just a fancy tool for just laying out our view but what we are going to do is to paste this canvas over top of the entire view and we are going to set the timer and everytime the timer goes off we are going to redraw over top of the view.
-
+        //scene builder is just the layout.... its just a fancy tool for just laying out our view but what we are going to do is to paste this canvas over top of the entire view and we are going to set the timer and everytime the timer goes off we are going to redraw over top of the view.(like animation).
 
         // a canvas can be used to "draw" on. The GraphicsContext is the tool used for the drawing.
-        Canvas canvas = new Canvas(GameConfig.getGame_width(),GameConfig.getGame_height());
+
+        //A canvas is not the scene  and it is an objext that gets added to the scene just like combobox is an object & label is an object...
 
         //when we draw on the canvas, we need some tools like pencil, marker, paintbrushes but in computer we called it the GraphicContext
+        Canvas canvas = new Canvas(GameConfig.getGame_width(),GameConfig.getGame_height());
         GraphicsContext gc = canvas.getGraphicsContext2D(); // this is like asking the canvas to draw on it.
 
 
---
+++ src/main/java/com/example/video_game/Sprite.java
@@ -0,0 +1,96 @@
+package com.example.video_game;
+
+import javafx.scene.image.Image;
+
+public abstract class Sprite {
+
+    private Image image;
+    private int posX,posY,imageWidth,imageHeight,speed;
+    private boolean alive;
+
+    /**
+     * This is the constructor for the Sprite class
+     * @param image -javafx image
+     * @param posX - the left most position of the Sprite
+     * @param posY- the top position of the Sprite
+     * @param imageWidth - the width of the image when drawn
+     * @param imageHeight- the height of the image when drawn
+     * @param speed- how many pixels the Sprite can move.
+     */
+    public Sprite(Image image, int posX, int posY, int imageWidth, int imageHeight, int speed) {
+        setImage(image);
+        setPosX(posX);
+        setPosY(posY);
+        setImageWidth(imageWidth);
+        setImageHeight(imageHeight);
+        setSpeed(speed);
+        alive = true;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    // there are not lot of validations for the image, its already an image object. Constructor for the image has already validated it.
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        //furthestRight -> our image can be drawn and still be visible on the canvas so as long as we're inside that range we're good to go.
+        int furthestRight = GameConfig.getGame_width() -imageWidth;
+        if(posX>=0 && posX<=furthestRight)
+            this.posX = posX;
+        else
+            throw new IllegalArgumentException("posX must be in the range of 0-"+furthestRight);
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        int furthestDown = GameConfig.getGame_height()-imageHeight;
+        if(posY>=0 && posY <= furthestDown)
+            this.posY = posY;
+        else
+            throw new IllegalArgumentException(String.format("posY must be in the range of 0-%d",furthestDown));
+        // exception always need a string in the bracket;
+    }
+
+    public int getImageWidth() {
+        return imageWidth;
+    }
+
+    public void setImageWidth(int imageWidth) {
+        this.imageWidth = imageWidth;
+    }
+
+    public int getImageHeight() {
+        return imageHeight;
+    }
+
+    public void setImageHeight(int imageHeight) {
+        this.imageHeight = imageHeight;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+}
